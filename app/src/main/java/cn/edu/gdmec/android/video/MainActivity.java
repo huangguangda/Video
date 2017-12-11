86329b8cbebcd791e710f588a4440afb85519dc7
huangguangda <2397923107@qq.com>
2017-12-11 21:27:41 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/video/MainActivity.java
@@ -0,0 +1,35 @@
+package cn.edu.gdmec.android.video;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+    }
+
+    public void intent(View view){
+        Uri uri = Uri.parse ( "http://android2017.duapp.com/beyond.mp4" );
+        Intent intent = new Intent ( Intent.ACTION_VIEW );
+        intent.setDataAndType ( uri,"video/mp4" );
+        startActivity ( intent );
+    }
+    public void videoview(View view){
+        Uri uri = Uri.parse ( Environment.getExternalStorageDirectory ().getAbsolutePath ()
+        + "/beyond.mp4");
+        VideoView videoView = (VideoView) findViewById ( R.id.videoview );
+        videoView.setMediaController ( new MediaController ( this ) );
+        videoView.setVideoURI ( uri );
+        videoView.start ();
+        videoView.requestFocus ();
+    }
+}
