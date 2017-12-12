23b1d078902cc65eca219d1272e268cf1ae9d87e
huangguangda <2397923107@qq.com>
2017-12-13 00:13:57 +0800

    Video play

--- app/src/main/java/cn/edu/gdmec/android/video/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/video/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
